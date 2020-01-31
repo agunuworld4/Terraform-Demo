8fb5f85a38181f4328a7cdba8fbc0cdb29c98f9b
agunuworld4 <59214742+agunuworld4@users.noreply.github.com>
2020-01-31 10:42:18 -0600

    Update variables.tf

--- app-service-and-sql-database-var/variables.tf
+++ app-service-and-sql-database-var/variables.tf
@@ -1,5 +1,5 @@
 variable "resource-group-name" {
-  default = "terraform-resource-group"
+  default = "myGroupResources"
   description = "The prefix used for all resources in this example"
 }
 
@@ -9,6 +9,6 @@ variable "app-service-name" {
 }
 
 variable "location" {
-  default = "West Europe"
+  default = "East US 2"
   description = "The Azure location where all resources in this example should be created"
 }
